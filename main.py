e00f6865137dc20031eee2d31d5a19b24d179819
PietroMaco <pietro.maco@gmail.com>
2020-04-26 21:12:15 +0200

    Add xlsx files

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 if __name__=="__main__":
-    print("ciao caccaß")
+    import xlrd
+
+    loc=("/Users/pietromacori/github/fantApp/Quotazioni_Fantacalcio_Test.xlsx")
+
+    wb=xlrd.open_workbook(loc)
+    sheet=wb.sheet_by_index(0)
+    sheet.cell_value(0,0)
+
+    for i in range(sheet.nrows):
+        for j in range(sheet.ncols):
+            print(sheet.cell_value(i,j)),
+
+        print("")
